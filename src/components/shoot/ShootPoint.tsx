6299753a82c4796b7dc410cda3788469028cfe52
unknown <pierremichel804@gmail.com>
2023-12-08 18:41:07 +0100

    Correction taille point shoot

--- src/components/shoot/ShootPoint.tsx
+++ src/components/shoot/ShootPoint.tsx
@@ -1,7 +1,8 @@
 import { Box } from "@mui/material";
-import { percent, px } from "csx";
+import { percent, px, rem } from "csx";
 import { Shoot } from "src/models/Shoot";
 import { getColorShoot } from "src/utils/calcul";
+import { getBreakpoint } from "src/utils/mediaQuery";
 
 interface Props {
   shoot: Shoot;
@@ -9,6 +10,8 @@ interface Props {
 }
 
 export const ShootPoint = ({ shoot, rotate }: Props) => {
+  const breakpoint = getBreakpoint();
+
   const color = getColorShoot(shoot.time);
   let x = shoot.x;
   let y = shoot.y;
@@ -22,6 +25,19 @@ export const ShootPoint = ({ shoot, rotate }: Props) => {
     }
   }
 
+  const getSize = () => {
+    let size = 12;
+    switch (breakpoint) {
+      case "xs":
+        size = 5;
+        break;
+      case "sm":
+        size = 8;
+        break;
+    }
+    return size;
+  };
+
   return (
     <Box
       sx={{
@@ -30,10 +46,10 @@ export const ShootPoint = ({ shoot, rotate }: Props) => {
         top: percent(y),
         transform: "translate(-50%, -50%)",
         backgroundColor: color,
-        width: px(12),
-        height: px(12),
+        width: px(getSize()),
+        height: px(getSize()),
         border: `3px solid ${color}`,
-        borderRadius: px(6),
+        borderRadius: percent(50),
         boxShadow: "0 0 0 2px #fff",
       }}
     />
